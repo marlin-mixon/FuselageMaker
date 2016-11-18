b0c9367c3d645238b3a168780bc970f68e9829c2
marlin-mixon <marlin.mixon@gmail.com>
2016-11-17 22:18:17 -0600

    Update directives.js

--- js/directives.js
+++ js/directives.js
@@ -2,26 +2,9 @@
 
 /* Directives */
 
-
 angular.module('fuselageMaker.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]).directive('newWindow', ['$window', '$compile', '$rootScope', function($window, $compile, $rootScope) {
-    return {
-      restrict: 'EA',
-      link: function($scope, $element, attr) {
-        $rootScope.window = $window.open('','_blank');
-        // $rootScope.window = $window.open('', '_blank', 'toolbar=1,scrollbars=1,location=1,status=1,menubar=1,resizable=1,width=300,height=550,left = 300,top=100').focus();
-        angular.element($rootScope.window.document.body).append($compile($element.contents())($scope));
-        // angular.element($rootScope.window.document.head).append($window.document.head);
-        $element.on('$destroy', function() {
-          $rootScope.window.close();
-        });
-      },
-      controller: function($scope, $element) {
-
-      }
-    }
   }]);
