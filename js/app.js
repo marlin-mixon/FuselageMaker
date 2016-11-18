4df1d8e593d0bc63b54391348f40991415c21df3
marlin-mixon <marlin.mixon@gmail.com>
2016-11-17 22:17:20 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,9 +8,4 @@ angular.module('fuselageMaker', [
   'fuselageMaker.services',
   'fuselageMaker.directives',
   'fuselageMaker.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+]);
