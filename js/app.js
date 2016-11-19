07faa75f77bf8b8ca5d9dd168374ac187e0fce9f
marlin-mixon <marlin.mixon@gmail.com>
2016-11-18 20:31:39 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('fuselageMaker', [
-  'ngRoute',
   'fuselageMaker.filters',
   'fuselageMaker.services',
   'fuselageMaker.directives',
